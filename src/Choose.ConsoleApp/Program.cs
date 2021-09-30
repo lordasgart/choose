84c28b7e855a68b0f4812757b287d5065c542c79
Markus Malz <markus.malz@outlook.com>
2021-09-30 17:54:33 +0200

    Removed test case since code didn't pass QA

---
+++ src/Choose.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Choose.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gib deine Optionen mit ENTER ein:");
+
+            List<string> options = new List<string>();
+            string option = string.Empty;
+            while (true)
+            {
+                option = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(option))
+                {
+                    break;
+                }
+                options.Add(option);
+            }
+
+            var random = RandomNumber.Between(0, options.Count);
+
+            Console.WriteLine("Deine Wahl fällt auf:");
+            Console.WriteLine(options[random]);
+            
+            Console.ReadLine();
+        }
+    }
+}
