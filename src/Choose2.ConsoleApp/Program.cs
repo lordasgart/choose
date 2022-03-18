41d3e0e890725d7a2b3e2f526d3fb08206ff7d47
Malz, Markus (ext) (DI FA CTR TE PRC1) <markus.malz.ext@siemens.com>
2022-03-18 11:18:58 +0100

    choose2 only outputs 0 and 1
    TODO: Write to file

---
+++ src/Choose2.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿while (true)
+{
+    var keyInfo = Console.ReadKey();
+    if (keyInfo.Key == ConsoleKey.Enter)
+    {
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.Write("\b");
+    }
+    var random = RandomNumber.Between(0, 1);
+    Console.Write(random);
+}
